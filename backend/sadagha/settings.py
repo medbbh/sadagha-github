c5940951c754af4b5c5d320d673fb3576bbe5eb4
medbbh <mbabah3450@gmail.com>
2025-10-13 16:20:08 +0000

    change to official domain

--- backend/sadagha/settings.py
+++ backend/sadagha/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://nextremitly-backend-fe52128f2003.herokuapp.com",  # Nextremitly backend
     "http://localhost:8001",  # Nextremitly microservice
     "https://sadagha.vercel.app",  # Production frontend
+    "https://www.sada9a.com"
 ]
 
 # Logging configuration for debugging
