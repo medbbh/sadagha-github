cc7967ccab5aac7a5a67cf54ca985f74c8e76c28
medbbh <mbabah3450@gmail.com>
2025-10-13 16:44:08 +0000

    add console hiding in prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  esbuild: {
+    drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
+  },
 })
